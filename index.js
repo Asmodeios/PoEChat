f6b8bfd4ee607c2f88a9240857bc62ed867ff894
Asmodei <wo1234@mail.ru>
2021-05-21 15:43:12 +0200

    add auto clear

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var express = require('express');
-var socket = require('socket.io');
-var Tail = require('tail').Tail;
-var http = require('http');
-var os = require('os');
+const express = require('express');
+const socket = require('socket.io');
+const Tail = require('tail').Tail;
+const http = require('http');
+const os = require('os');
 require('dotenv').config();
 
-var options= {separator: /[\r]{0,1}\n/, fromBeginning: false, fsWatchOptions: {}, follow: true, logger: console, useWatchFile: true}
+const options= {separator: /[\r]{0,1}\n/, fromBeginning: false, fsWatchOptions: {}, follow: true, logger: console, useWatchFile: true}
 
-var tail = new Tail(process.env.CLIENT_PATH, options);
-var networkInterfaces = os.networkInterfaces();
-var localIP;
+const tail = new Tail(process.env.CLIENT_PATH, options);
+const networkInterfaces = os.networkInterfaces();
+let localIP;
 
 for (const name of Object.keys(networkInterfaces)) {
   for (const networks of networkInterfaces[name]) {
@@ -21,15 +21,15 @@ for (const name of Object.keys(networkInterfaces)) {
   }
 }
 
-var app = express();
-var server = http.createServer(app);
+const app = express();
+const server = http.createServer(app);
 server.listen(4000, localIP, () => {
   console.log(`link to chat: ${localIP}:4000`);
 });
 
 app.use(express.static('public'));
 
-var io = socket(server);
+const io = socket(server);
 
 io.on('connection', (socket) => {
   console.log('socket connection', socket.id);
--- public/table.js
+++ public/table.js
@@ -1,9 +1,25 @@
-var socket = io.connect(`ws://${window.location.host}`);
+const socket = io.connect(`ws://${window.location.host}`);
 let $list = $('#list');
 
 const clearChat = () => {
   $list.empty();
-}
+};
+
+var autoClearInterval;
+
+const autoClear = () => {
+  const value = $('#auto-clear').val();
+  if (value) {
+    if (autoClearInterval) {
+      clearInterval(autoClearInterval);
+    }
+    const minutes = +value * 1000 * 60;
+    autoClearInterval = setInterval(() => {
+      $list.empty();
+    }, minutes)
+  }
+  return;
+} 
 
 socket.on('newMessage', (message) => {
   let rgx = /(?<time>\d{2}:\d{2}:\d{2})(?:.+].+)(?<text>(@To|@From).+)/;
@@ -20,7 +36,7 @@ socket.on('newMessage', (message) => {
     html: time,
   })).css({'display': 'flex', 'gap': '1rem'});
 
-  const isBuy = /^@From.+Hi, I would|I'd like to buy your.+/.test(text);
+  const isBuy = /^@From|To.+Hi, I would|I'd like to buy your.+/.test(text);
 
   $element.append($('<div>', {
     'class': isBuy ? 'buy' : 'text',
